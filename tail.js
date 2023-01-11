75259d8fb00c22dab935ef0c8333ce13fd85a2a3
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-11 11:01:42 -0500

    tail.js fixed

--- tail.js
+++ tail.js
@@ -1,5 +1,8 @@
 const tail = function(arr){
+  let tailArray = []
   if(arr.length === 0)
     return undefined;
-  return arr[arr.length - 1];
+  for(let i = 0; i < arr.length; i++)
+    tailArray.push(arr[i]);
+  return tailArray;
 }
