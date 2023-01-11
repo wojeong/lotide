c96f3aef98ed800f4d9b7fd3f2ff752df8766fbf
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-11 13:02:13 -0500

    countOnly.js done

---
+++ countOnly.js
@@ -0,0 +1,33 @@
+const countOnly = function(allItems, itemsToCount){
+  const results = {};
+  for(const item of allItems){
+    for(const name in itemsToCount){
+      if(item === name && itemsToCount[name]){
+        if(!results[name])
+          results[name] = 0;
+        results[name]++;  
+      }
+    }
+  }
+  return results;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+//assertEqual(result1["Jason"], 1);
+//assertEqual(result1["Karima"], undefined);
+//assertEqual(result1["Fang"], 2);
+//assertEqual(result1["Agouhanna"], undefined);
