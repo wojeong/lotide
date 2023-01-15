658a889ebbf3438bbcf36e5b0d0f7ebb4dc21ac0
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-15 17:23:04 -0500

    findKey.js done

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected){
+  if(actual === expected){
+    console.log(true);
+  }
+  else
+    console.log(false);
+}
+
+const findKey = function(object, callback){
+  for (let index in object) {
+    if(callback(object[index]))
+      return index;
+  }
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma") // => "noma"
+
