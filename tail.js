49b7d8a3f368645153ef35e58cdc164860c973a9
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-17 00:00:56 -0500

    tail.js refractor

--- tail.js
+++ tail.js
@@ -2,7 +2,9 @@ const tail = function(arr){
   let tailArray = []
   if(arr.length === 0)
     return undefined;
-  for(let i = 0; i < arr.length; i++)
+  for(let i = 1; i < arr.length; i++)
     tailArray.push(arr[i]);
   return tailArray;
 }
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,4 @@
+const tail = require('../tail');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
